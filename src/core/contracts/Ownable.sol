47ef37890b3f989136682e2be682cace20424c45
Wondeuk Yoon <wdyoon@kaist.ac.kr>
2018-03-23 01:51:48 +0900

    processing for ABLE DEX

--- src/core/contracts/Ownable.sol
+++ src/core/contracts/Ownable.sol
@@ -4,32 +4,32 @@
  * functions, this simplifies the implementation of "user permissions". 
  */
 contract Ownable {
-  address public owner;
+	address public owner;
 
-  /** 
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  function Ownable() {
-    owner = msg.sender;
-  }
+	/** 
+	 * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+	 * account.
+	 */
+	function Ownable() {
+		owner = msg.sender;
+	}
 
-  /**
-   * @dev Throws if called by any account other than the owner. 
-   */
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
+	/**
+	 * @dev Throws if called by any account other than the owner. 
+	 */
+	modifier onlyOwner() {
+		require(msg.sender == owner);
+		_;
+	}
 
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to. 
-   */
-  function transferOwnership(address newOwner) onlyOwner public {
-    if (newOwner != address(0)) {
-      owner = newOwner;
-    }
-  }
+	/**
+	 * @dev Allows the current owner to transfer control of the contract to a newOwner.
+	 * @param newOwner The address to transfer ownership to. 
+	 */
+	function transferOwnership(address newOwner) onlyOwner public {
+		if (newOwner != address(0)) {
+			owner = newOwner;
+		}
+	}
 
 }
